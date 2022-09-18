02cd3b83e911d4de81c58fe7535fe4ca6bd2b677
osajab10@gmail.com <osajab10@gmail.com>
2022-09-18 13:26:59 +0200

    Add project files.

---
+++ TwoDecksWPF/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDecksWPF
+{
+    class Card
+    {
+        public Values Value { get; private set; }
+        public Suits Suit { get; private set; }
+
+        public Card(Values value, Suits suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+
+        public string Name
+        {
+            get { return $"{Value} of {Suit}"; }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ TwoDecksWPF/Deck.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace TwoDecksWPF
+{
+    class Deck : ObservableCollection<Card>
+    {
+        private static Random random = new Random();
+
+        public Deck()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Clear();
+            for(int suit = 0; suit <=3; suit++)
+            {
+                for(int value=0; value <= 13; value++)
+                {
+                    Add(new Card((Values)value, (Suits)suit));
+                }
+            }
+        }
+
+        //public Card Deal(int index)
+        //{
+        //    Card cardToDeal = base[index];
+        //    RemoveAt(index);
+        //    return cardToDeal;
+        //}
+
+        public void Shuffle()
+        {
+            List<Card> copy = new List<Card>(this); 
+            Clear(); 
+            while(copy.Count > 0)
+            {
+                int index = random.Next(copy.Count);
+                Card card = copy[index];
+                copy.RemoveAt(index);
+                Add(card);
+            }
+        }
+
+        public void Sort()
+        {
+            List<Card> sortedCards = new List<Card>(this);
+            sortedCards.Sort(new CardComparerByValue());
+            Clear();
+            foreach(Card card in sortedCards)
+            {
+                Add(card);
+            }
+        }
+    }
+}
